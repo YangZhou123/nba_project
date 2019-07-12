bdcbb35e67b64e1b7694df18e938d3535ccc5d62
Yang Zhou <yza358@sfu.ca>
2019-07-11 18:12:33 -0700

    add all code about kawhi

---
+++ data_analsis_kawhi/crawl_data_tr.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import collections
+import time
+import csv
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://stats.nba.com/team/1610612761/boxscores/')
+
+table0 = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div/nba-stat-table/div[2]/div[1]/table')
+print(table0.text)
+with open('tdata0.txt', 'w') as f:
+    f.write(table0.text)
+
+button = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div/nba-stat-table/div[3]/div/div/a[2]/i')
+button.click()
+
+table1 = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[3]/div/div/div/nba-stat-table/div[2]/div[1]/table')
+print(table0.text)
+with open('tdata1.txt', 'w') as f:
+    f.write(table1.text)
+
+
---
+++ data_analsis_kawhi/data_processing.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv('data0.txt', sep="\s*")
+print(df)
